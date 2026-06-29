765a6750fbbfe1a0e972729be56bbb7acf9acbb3
theflysong <song_of_the_fly@163.com>
2026-06-30 07:54:43 +0800

    update basic testcases for loongarch

--- module/contest-runner/basic.h
+++ module/contest-runner/basic.h
@@ -1,5 +1,4 @@
 namespace basic {
-#if defined(__ARCH_riscv64__)
     const char *testcases[] = {
         "brk",    "chdir",   "clone",        "close",  "dup2",   "dup",
         "execve", "exit",    "fork",         "fstat",  "getcwd", "getdents",
@@ -7,14 +6,4 @@ namespace basic {
         "munmap", "openat",  "open",         "pipe",   "read",   "sleep",
         "times",  "umount",  "uname",        "unlink", "wait",   "waitpid",
         "write",  "yield",   nullptr};
-#elif defined(__ARCH_loongarch64__)
-    // remove "sleep" for loongarch64 to improve stability
-    const char *testcases[] = {
-        "brk",    "chdir",   "clone",        "close",  "dup2",    "dup",
-        "execve", "exit",    "fork",         "fstat",  "getcwd",  "getdents",
-        "getpid", "getppid", "gettimeofday", "mkdir_", "mmap",    "mount",
-        "munmap", "openat",  "open",         "pipe",   "read",    "times",
-        "umount", "uname",   "unlink",       "wait",   "waitpid", "write",
-        nullptr};
-#endif
 }  // namespace basic
